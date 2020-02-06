ddccb5d0015e1f95cad9facdffe2c0629c3c84a7
Mark Kooijman <kooi.barlaeus@gmail.com>
2020-02-06 20:39:42 +0100

    Create daniel-turtlecluster.py

---
+++ daniel-turtlecluster.py
@@ -0,0 +1,60 @@
+"""(c) Daniel 2020"""
+
+import turtle
+a = turtle.Turtle()
+a.shape("turtle")
+a.speed(10)
+a.pencolor ("light blue")
+def b (c, d, e, f, g, h):
+    for i in range (36):
+        a.circle(20)
+        a.left (10)
+    for z in range (2):
+        a.getscreen ().bgcolor(c)
+        a.left(40)
+        a.getscreen ().bgcolor(h)
+        a.left(40)
+        a.getscreen ().bgcolor(e)
+        a.left(40)
+        a.getscreen ().bgcolor(f)
+        a.left(40)
+        a.getscreen ().bgcolor(g)
+        a.left(40)
+        a.getscreen ().bgcolor(d)
+        a.left(40)
+b ("red", "orange", "purple", "green", "blue", "yellow")
+import turtle
+tina = turtle.Turtle()
+tina.shape("turtle")
+tina.speed(200)
+import turtle
+nita = turtle.Turtle()
+nita.shape("turtle")
+nita.speed(200)
+import turtle
+nat1 = turtle.Turtle()
+nat1.shape("turtle")
+nat1.speed(140)
+tina.left(60)
+a.left(180)
+nat1.left(240)
+def wan(leng, kleur2, ker, kleur1, aan, kleur3, kleur4):
+    for i in range(aan):
+        tina.left(180)
+        nita.right(180)
+        a.left(180)
+        nat1.right(180)
+        for i in range(ker):
+            tina.pencolor(kleur1)
+            nita.pencolor(kleur2)
+            nita.forward(leng)
+            tina.forward(leng)
+            tina.right(60)
+            nita.left(60)
+            a.pencolor(kleur3)
+            a.forward(leng)
+            a.left(60)
+            nat1.pencolor(kleur4)
+            nat1.forward(leng)
+            nat1.left(60)
+wan(30, "red", 3, "blue", 6, "green", "purple")
