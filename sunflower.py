1380223264fb04f602175883ada76f1a79622682
Mark Kooijman <kooi.barlaeus@gmail.com>
2025-02-04 21:12:28 +0100

    Update sunflower.py

--- sunflower.py
+++ sunflower.py
@@ -5,22 +5,19 @@ tina = turtle.Turtle()
 tina.shape('turtle')
 tina.speed(0)
 tina.penup()
-tina.getscreen().colormode(255)
 tina.hideturtle()
+turtle.Screen().tracer(10)
 
 a0 = 137.5
-c = 4
+c = 8
 
 step = 1
 
-#for n in range(1000):
-#for c1 in range(0, 256, step):
 for c2 in range(0, 256, 32):
-    print(c2)
     for c3 in range(1, 256, step):
         n = c3
         angle = n * a0
         r = c * math.sqrt(n)
         tina.goto( r*math.cos(angle), r*math.sin(angle) )
-        tina.pencolor(0, c2, c3)
+        tina.pencolor(256-c2, c2, c3)
         tina.dot()
